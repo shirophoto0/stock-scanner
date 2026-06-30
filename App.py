e08668a31ae7e7ad88cb2d3c12564ab4357af302
shirophoto0 <shirophoto0@gmail.com>
2026-06-30 21:58:37 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2231,6 +2231,11 @@ with tab_tfex:
         tfex_df = load_data("TFEX_History") # ฟังก์ชันโหลดข้อมูลเดิมของพี่อ้ำ
         
         if not tfex_df.empty:
+            st.write(tfex_df.columns)
+            required_cols = ['Net_Profit', 'Win_Lose']
+                for col in required_cols:
+                    if col not in tfex_df.columns:
+                        tfex_df[col] = 0.0 # ถ้าไม่มีให้เติมเป็น 0 ไปก่อน
             # 2. คำนวณ Cumulative Profit
             tfex_df['Cumulative_Profit'] = tfex_df['Net_Profit'].cumsum()
             
